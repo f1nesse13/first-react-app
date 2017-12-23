0cc39382fbbea0c3e5d8f17cf88bfb48fb0afa56
Joseph Pell <philly.teg@gmail.com>
2017-12-22 21:48:46 -0500

    added conditionals and used map to create a list

--- src/App.js
+++ src/App.js
@@ -9,8 +9,9 @@ class App extends Component {
 
     this.state = {
       persons: [
-        { name: "Joe", age: "30", dogName: "Aurora", dogAge: "2" },
-        { name: "Tim", age: "21", dogName: "booBop", dogAge: "13" }
+        { id: "d328nc747", name: "Joe", age: "30", dogName: "Aurora", dogAge: "2" },
+        { id: "d209189c3", name: "Vincent", age: "27", dogName: "Spot", dogAge: "8" },
+        { id: "d8324e092", name: "Elizabeth", age: "23", dogName: "Cupcake", dogAge: "16" }
       ],
       showPerson: false
     }
@@ -18,18 +19,22 @@ class App extends Component {
   nameEventHandler = (newName) => {
     this.setState({
       persons: [
-        { name: newName, age: "45", dogName: "Golfy", dogAge: "5002" },
-        { name: "Santa", age: "73", dogName: "Jesus", dogAge: "6" }
+        { id: "d328nc747", name: newName, age: "45", dogName: "Golfy", dogAge: "5002" },
+        { id: "d209189c3", name: "Santa", age: "73", dogName: "Jesus", dogAge: "6" },
+        { id: "d8324e092", name: "Easter Bunny", age: "37", dogName: "newName", dogAge: "62" }
       ]
     })
   }
-  inputHandler = (event) => {
-    this.setState({
-      persons: [
-        { name: "Joe" , age: "30", dogName: event.target.value, dogAge: "2" },
-        { name: event.target.value, age: "21", dogName: "Aurora", dogAge: "13" }
-      ]
+  inputHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id; // Finds the right person accordind to their ID
     })
+    const person = { ...this.state.persons[personIndex] } // makes copy of the person at correct index
+    person.name = event.target.value
+    // Captured value of input and set equal to person.name
+    const persons = [...this.state.persons] // Copy of state
+    persons[personIndex] = person // sets the states persons array = to the person found
+    this.setState({persons: persons}) // sets state to the new value of persons
   }
   deletePersonHandler = (personIndex) => {
     const peeps = [...this.state.persons]
@@ -46,9 +51,11 @@ class App extends Component {
       persons = (
       <div>
       {this.state.persons.map((person, index) => {
-        return <Person
+        return <Person 
+        key={person.id}
+        inputChange={( event ) => this.inputHandler( event, person.id )}
         click={() => this.deletePersonHandler(index)}
-         name={person.name} age={person.name}
+         name={person.name} age={person.age}
           dogName={person.dogName} dogAge={person.dogAge} />
       })}
       </div>
